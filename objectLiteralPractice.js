e6d7ba88460974726758a53d64c6cd55769d6727
Drew Bowling <andrewwbowling@gmail.com>
2023-03-03 07:20:32 -0700

    complete pairing excercise 1

---
+++ objectLiteralPractice.js
@@ -0,0 +1,14 @@
+var person = {
+    name: "Ronald Acuna",
+    placeOfBirth: "Venezuala",
+    isAllStar: true,
+    jerseyNumber: 13
+};
+
+person.throws = "Right";
+person.homeRuns = 120;
+
+
+console.log(person);
+
+
